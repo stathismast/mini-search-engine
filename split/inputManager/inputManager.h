0740f655aa237b4c225810f366504977f9ca8305
stathismast <Sdi1500092@di.uoa.gr>
2018-03-08 19:13:52 +0200

    Delete inputManager.h

--- split/inputManager/inputManager.h
+++
@@ -1,9 +0,0 @@
-#include "../charList/charList.h"
-#include "lineInfo.h"
-#include "../trie/trie.h"
-#include <stdio.h>
-
-int firstRead(FILE * input, LineInfo ** head);
-char ** secondRead(FILE * input, int lineCounter, LineInfo * head);
-char ** readInputFile(char * fileName, int * lineCounter);
-int manageArguments(int argc, char *argv[], char * fileName, int * k);
